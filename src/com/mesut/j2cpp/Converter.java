5e7a8090e82fc966bf56dd67c3500592ba971e68
Mesut <mesutdogansoy@gmail.com>
2019-12-24 15:45:13 +0300

    reformat

--- src/com/mesut/j2cpp/Converter.java
+++ src/com/mesut/j2cpp/Converter.java
@@ -21,24 +21,24 @@ public class Converter {
     String dest;
     String sysPath;
 
-    public Converter(String src,String dest) {
-        this.src=src;
-        this.dest=dest;
+    public Converter(String src, String dest) {
+        this.src = src;
+        this.dest = dest;
     }
 
     public Resolver getResolver() {
         return resolver;
     }
 
-    public void convert(){
+    public void convert() {
         //makeTable();
-        convertDir(new File(src),"");
+        convertDir(new File(src), "");
     }
 
     public void makeTable() {
-        table=new SymbolTable();
-        resolver=new Resolver(table);
-        File dir=new File(src);
+        table = new SymbolTable();
+        resolver = new Resolver(table);
+        File dir = new File(src);
         //tableDir(dir);
         //System.out.println(table.list.size());
         /*for (Symbol s:table.list) {
@@ -46,82 +46,84 @@ public class Converter {
         }*/
     }
 
-    void tableDir(File dir){
-        for (File file:dir.listFiles()){
-            if (file.isFile()){
-                if (file.getName().endsWith(".java")){
+    void tableDir(File dir) {
+        for (File file : dir.listFiles()) {
+            if (file.isFile()) {
+                if (file.getName().endsWith(".java")) {
                     try {
-                        CompilationUnit cu=StaticJavaParser.parse(file);
-                        //add
-                        for(TypeDeclaration<?> type:cu.getTypes()){
-                            if (type.isClassOrInterfaceDeclaration()){
-                                tableClass(type,cu);
+                        CompilationUnit cu = StaticJavaParser.parse(file);
+                        // cu,pkg,name
+                        for (TypeDeclaration<?> type : cu.getTypes()) {
+                            if (type.isClassOrInterfaceDeclaration()) {
+                                tableClass(type, cu);
                             }
                         }
                     } catch (FileNotFoundException e) {
                         e.printStackTrace();
                     }
                 }
-            }else {
+            } else {
                 tableDir(file);
             }
         }
     }
 
-    void tableClass(TypeDeclaration<?> type,CompilationUnit cu){
-        if (cu.getPackageDeclaration().isPresent()){
-            table.addSymbol(cu.getPackageDeclaration().get().getNameAsString(),type.getNameAsString());
-        }else {
-            table.addSymbol("",type.getNameAsString());
+    void tableClass(TypeDeclaration<?> type, CompilationUnit cu) {
+        if (cu.getPackageDeclaration().isPresent()) {
+            table.addSymbol(cu.getPackageDeclaration().get().getNameAsString(), type.getNameAsString());
+        } else {
+            table.addSymbol("", type.getNameAsString());
         }
-        type.getMembers().forEach(m->{
-            if (m.isClassOrInterfaceDeclaration()){
-                tableClass(m.asClassOrInterfaceDeclaration(),cu);
+        type.getMembers().forEach(m -> {
+            if (m.isClassOrInterfaceDeclaration()) {
+                tableClass(m.asClassOrInterfaceDeclaration(), cu);
             }
         });
     }
 
-    public void convertDir(File dir,String pkg){
-        for (File file:dir.listFiles()){
-            if (file.isFile()){
-                if (file.getName().endsWith(".java")){
-                    convertSingle(file,pkg);
+    public void convertDir(File dir, String pkg) {
+        for (File file : dir.listFiles()) {
+            if (file.isFile()) {
+                if (file.getName().endsWith(".java")) {
+                    convertSingle(file, pkg);
                 }
-            }else {
-                convertDir(file,pkg+"/"+file.getName());
+            } else {
+                convertDir(file, pkg + "/" + file.getName());
             }
         }
     }
-    public void convertSingle(File file,String pkg){
+
+    public void convertSingle(File file, String pkg) {
         try {
-            String path=pkg+"/"+file.getName();
+            String path = pkg + "/" + file.getName();
 
-            CHeader header=new CHeader();
-            CSource cpp=new CSource(header);
-            header.rpath=path;
+            CHeader header = new CHeader();
+            CSource cpp = new CSource(header);
+            header.rpath = path;
 
-            MainVisitor visitor=new MainVisitor(this,header);
+            MainVisitor visitor = new MainVisitor(this, header);
 
-            CompilationUnit cu=StaticJavaParser.parse(file);
-            cu.accept(visitor,null);
+            CompilationUnit cu = StaticJavaParser.parse(file);
+            cu.accept(visitor, null);
 
-            String hs=header.toString();
-            String ss=cpp.toString();
+            String hs = header.toString();
+            String ss = cpp.toString();
             System.out.println(hs);
             System.out.println("---------------");
             System.out.println(ss);
-            File fcpp=new File(dest,path.replace(".java",".cpp"));
+            File fcpp = new File(dest, path.replace(".java", ".cpp"));
             fcpp.getParentFile().mkdirs();
-            Files.write(Paths.get(fcpp.getAbsolutePath()),ss.getBytes());
+            Files.write(Paths.get(fcpp.getAbsolutePath()), ss.getBytes());
 
-            File fh=new File(dest,path.replace(".java",".h"));
-            Files.write(Paths.get(fh.getAbsolutePath()),hs.getBytes());
+            File fh = new File(dest, path.replace(".java", ".h"));
+            Files.write(Paths.get(fh.getAbsolutePath()), hs.getBytes());
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public void convertSingle(String cls){
-        convertSingle(new File(src,cls),"");
+
+    public void convertSingle(String cls) {
+        convertSingle(new File(src, cls), "");
     }
 
 }
